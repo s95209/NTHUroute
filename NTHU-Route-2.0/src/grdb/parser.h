39d8c23a50ceddcfc167f37d8ec72268a3a908f1
s95209 <s95209louie@gmail.com>
2024-03-20 22:12:24 +0800

    feat: remove Parser (09 98 18)

--- NTHU-Route-2.0/src/grdb/parser.h
+++ NTHU-Route-2.0/src/grdb/parser.h
@@ -26,91 +26,39 @@ public:
   virtual void parse(Builder *builder) = 0;
 };
 
-/**
- * @brief Global Router Parser for parsing ISPD'07 test case
- */
-class Parser07 : public GRParser
-{
-  string fname_; ///< File name
-  string delims_;
-  FileHandler fh_;   ///< File Handler
-  Builder *builder_; ///< Data stucture builder
-public:
-  Parser07(const char *fname, FileHandler::FileType ftype);
-  virtual ~Parser07();
-  virtual void parse(Builder *builder);
-
-private:
-  /// Parse information of routing layers, tiles
-  void parseRoutingRegion();
-
-  /// Parse information of nets
-  void parseNets();
-
-  /// Parse information of one net
-  void parseANet();
-
-  /// Parse information of adjustmenting edge capacity
-  void adjustCapacity();
-};
-
-/**
- * @brief Global Router Parser for parsing ISPD'98 test case
- */
-class Parser98 : public GRParser
-{
-  string fname_; ///< File name
-  string delims_;
-  FileHandler fh_;   ///< File Handler
-  Builder *builder_; ///< Data stucture builder
-public:
-  Parser98(const char *fname, FileHandler::FileType ftype);
-  virtual ~Parser98();
-
-  virtual void parse(Builder *builder);
-
-private:
-  /// Parse the information of routing layers, tiles
-  void parseRoutingRegion();
-
-  /// Parse the information of nets
-  void parseNets();
-
-  /// Parse information of one net
-  void parseANet();
-};
-
-class Parser18 : public GRParser
-{
-  Builder *builder_;
-  std::shared_ptr<std::vector<std::vector<int>>> layerOneCap;
-
-public:
-  Parser18(std::shared_ptr<std::vector<std::vector<int>>> &cap) : layerOneCap(cap)
-  {
-  }
-  virtual ~Parser18();
-  virtual void parse(Builder *builder);
-
-  // void setLayerOneCap(int x, int y) { this->layerOneCap.resize(x, vector<int>(y, 0)); }
-  //  std::vector<std::vector<int>> getLayerOneCap() { return layerOneCap; };
-
-private:
-  void parseRoutingRegion();
-
-  /// Parse information of nets
-  void parseNets();
-
-  /// Parse information of one net
-  void parseANet(int index);
-
-  /// Parse information of adjustmenting edge capacity
-  void setCapacity();
 
 
-  void parseCmap();
-  void parseNetList();
-};
+// class Parser18 : public GRParser
+// {
+//   Builder *builder_;
+//   std::shared_ptr<std::vector<std::vector<int>>> layerOneCap;
+
+// public:
+//   Parser18(std::shared_ptr<std::vector<std::vector<int>>> &cap) : layerOneCap(cap)
+//   {
+//   }
+//   virtual ~Parser18();
+//   virtual void parse(Builder *builder);
+
+//   // void setLayerOneCap(int x, int y) { this->layerOneCap.resize(x, vector<int>(y, 0)); }
+//   //  std::vector<std::vector<int>> getLayerOneCap() { return layerOneCap; };
+
+// private:
+//   void parseRoutingRegion();
+
+//   /// Parse information of nets
+//   void parseNets();
+
+//   /// Parse information of one net
+//   void parseANet(int index);
+
+//   /// Parse information of adjustmenting edge capacity
+//   void setCapacity();
+
+
+//   void parseCmap();
+//   void parseNetList();
+// };
 
 class NetISPD24 {
     public:
@@ -141,19 +89,3 @@ class Parser24 : public GRParser
   virtual ~Parser24();
   virtual void parse(Builder *builder);
 };
-
-//======= Inline Functions =======
-
-inline Parser07::Parser07(const char *fname, FileHandler::FileType ftype)
-    : fname_(fname),
-      delims_(" \t\n"),
-      fh_(fname, ftype)
-{
-}
-
-inline Parser98::Parser98(const char *fname, FileHandler::FileType ftype)
-    : fname_(fname),
-      delims_(" \t\n"),
-      fh_(fname, ftype)
-{
-}
